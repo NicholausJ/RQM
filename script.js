57c3f74080b1868168faeaa2fd0557e682ef6128
Nick J <nicholausj@gmail.com>
2018-10-17 13:06:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //JS by Nick Jarmakovitz
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction1() {
+  var x = document.getElementById("my-topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 var displayQuote;
 var currentIndex;
 //var index;
